3722250333f4998316797e56a0c4971e84d2b76b
Aron Mucuri <aronmucuri@gmail.com>
2025-03-10 13:05:50 +0300

    Implementing authentication with jwt

---
+++ Digital_Banking_User_service/src/main/java/com/emtech/Digital_Banking_User_service/DigitalBankingUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.emtech.Digital_Banking_User_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBankingUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBankingUserServiceApplication.class, args);
+	}
+
+}
---
+++ Digital_Banking_User_service/src/test/java/com/emtech/Digital_Banking_User_service/DigitalBankingUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emtech.Digital_Banking_User_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankingUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
